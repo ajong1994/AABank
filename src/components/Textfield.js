d6c9ec452afbc2637af7bf5763e64e01ad0b7ec7
Arianne <amcanlubo@gmail.com>
2021-09-04 12:40:52 +0800

    edited prop in Textfield.js

--- src/components/Textfield.js
+++ src/components/Textfield.js
@@ -1,9 +1,13 @@
 
-const Textfield = ({children, id, classnames, placeholder, type}) => {
+const Textfield = ({children, id, classnames, placeholder, type, required }) => {
+
+    // const required = (required === true ) ? true : false
+
     return (
         <>
         <label htmlFor = {id}>{children}</label>
-        <input type={type} name ={id} id={id} className={classnames} placeholder={placeholder}/>
+        <input type={type} name ={id} id={id} className={classnames} placeholder={placeholder} required={required}/>
+                                                                                               
         </>
     )
 }
