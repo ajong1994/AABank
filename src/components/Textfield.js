d74057c36c36116e48a0ca8af4fb96dcd4b21740
Arianne <amcanlubo@gmail.com>
2021-09-04 12:46:25 +0800

    edited a prop in Textfield.js

--- src/components/Textfield.js
+++ src/components/Textfield.js
@@ -6,8 +6,7 @@ const Textfield = ({children, id, classnames, placeholder, type, required }) => 
     return (
         <>
         <label htmlFor = {id}>{children}</label>
-        <input type={type} name ={id} id={id} className={classnames} placeholder={placeholder} required={required}/>
-                                                                                               
+        <input type={type} name ={id} id={id} className={classnames} placeholder={placeholder} required={required}/>                                                                                
         </>
     )
 }
