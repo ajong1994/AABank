35b02f37825a5ce3e496e9f09c28ee4b3da7ec44
Arianne <amcanlubo@gmail.com>
2021-09-09 19:06:31 +0800

    Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Header from '../parts/Header'
 import Textfield from '../components/Textfield'
-import Error from '../components/Error'
+
 
 
 const Register = ({status}) => {
 
   const history = useHistory()
-
 
 // If currentAdmin is not amcanlubo or ajong, do not authorize to register other accounts
   if(status.isLoggedIn){
@@ -22,178 +21,53 @@ const Register = ({status}) => {
   }
 }
 
-const [userName, setuserName] = useState('')
-const [firstName, setfirstName] = useState('')
-const [lastName, setlastName] = useState('')
-const [passWord, setpassWord] = useState('')
-
-
 const [error, setError] = useState({
   usernameErr   : '',
   firstnameErr  : '',
   lastnameErr   : '',
   passwordErr   : ''
+  // isFormValid   : false
 })
 
-      console.log(userName)
-      console.log(error.usernameErr)
+const [userName, setuserName] = useState('')
+const [firstName, setfirstName] = useState('')
+const [lastName, setlastName] = useState('')
+const [passWord, setpassWord] = useState('')
+
+console.log(userName)
+console.log(error.usernameErr)
 
 let user_exist,
-form_valid;
+    form_valid
 
 //Regular expression for pattern matching to check 
 const validNameRegex = RegExp(/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?0-9]+/);
 
-
 //local storage data
 let adminInfo = {};
 
-
 // validate user input
-function validateUser() {
-
-  let saved_user = JSON.parse(localStorage.getItem(userName)) 
+const validateUser = (e) => {
+  
+  let username = e.target.value
+  let saved_user = JSON.parse(localStorage.getItem(username))
   
   if (saved_user !== null) {
-    user_exist = 1
+    form_valid =false
+    user_exist = true
   } else {  
-    user_exist = 0
+    user_exist = false
   }
-  console.log(user_exist)
+ 
 
 return user_exist
-  //   //=======USERNAME========/
-    
-  //     if (userName === '') {
-  //       // setError({
-  //       //   usernameErr: true,
-  //       //   firstnameErr : false,
-  //       //   lastameErr : false,
-  //       //   passwordErr : false
-          
-  //       // })
-  //       // seterrorMessage({usernameErr   : 'Please fill username field'})
-  //       // setError({usernameErr   : 'Please fill username field'})
-      
-  //     } else if (userName.length <  2 ) {
-  //       // setError({
-  //       //   usernameErr: true,
-  //       //   firstnameErr : false,
-  //       //   lastameErr : false,
-  //       //   passwordErr : false,
-          
-  //       // })
-  //       // seterrorMessage({usernameErr   : 'Username must be at least 2 characters long'})
-  //       // setError({usernameErr   : 'Username must be at least 2 characters long'})
-      
-  //     } else {
-  //       console.log('Username is valid')
-  //     }
-  //     //=======FIRSTNAME========/
-  //     if (firstName === '') {
-  //       // setError({
-  //       //   usernameErr: false,
-  //       //   firstnameErr : true,
-  //       //   lastameErr : false,
-  //       //   passwordErr : false
-          
-  //       // })
-  //       // seterrorMessage({firstnameErr   : 'Please fill firstname field!'})
-  //       setError({firstnameErr   : 'Please fill firstname field!'})
-  //     } else if (firstName.length < 2) {
-  //       // setError({
-  //       //   usernameErr: false,
-  //       //   firstnameErr : true,
-  //       //   lastameErr : false,
-  //       //   passwordErr : false
-          
-  //       // })
-  //       // setError({firstnameErr   : 'First name must be at least 2 characters long'})
-  //       // seterrorMessage({firstnameErr   : 'First name must be at least 2 characters long'})
-  //     } else if (validNameRegex.test(firstName)) {
-  //       // setError({
-  //       //   usernameErr: false,
-  //       //   firstnameErr : true,
-  //       //   lastameErr : false,
-  //       //   passwordErr : false
-          
-  //       // })
-  //       setError({firstnameErr   : 'Must not include numbers and special characters!'})
-  //       // seterrorMessage({firstnameErr   : 'Must not include numbers and special characters!'})
-  //     } else  {
-  //       console.log ('Firstname valid')
-  //     }
-  //     //=======LASTNAME========/
-  //     if (lastName === '') {
-  //       // setError({
-  //       //   usernameErr: false,
-  //       //   firstnameErr : false,
-  //       //   lastameErr : true,
-  //       //   passwordErr : false
-          
-  //       // })
-  //       setError({lastnameErr   : 'Please fill firstname field!'})
-  //       // seterrorMessage({lastnameErr   : 'Please fill firstname field!'})
-  //     } else if (lastName.length < 2) {
-  //       // setError({
-  //       //   usernameErr: false,
-  //       //   firstnameErr : false,
-  //       //   lastameErr : true,
-  //       //   passwordErr : false
-          
-  //       // })
-  //       // seterrorMessage({lastnameErr   : 'Last name must be at least 2 characters long'})
-  //       setError({lastnameErr   : 'Last name must be at least 2 characters long'})
-  //     } else if (validNameRegex.test(lastName)) {
-  //       // setError({
-  //       //   usernameErr: false,
-  //       //   firstnameErr : false,
-  //       //   lastameErr : true,
-  //       //   passwordErr : false
-          
-  //       // })
-  //       // seterrorMessage({lastnameErr   : 'Must not include numbers and special characters!'})
-  //       setError({lastnameErr   : 'Must not include numbers and special characters!'})
-  //     } else {
-  //       console.log ('Lastname valid')
-  //     }
+} 
 
-  //     //=======PASSWORD========/
-  //     if (passWord==='') {
-  //       // setError({
-  //       //   usernameErr: false,
-  //       //   firstnameErr : false,
-  //       //   lastameErr : false,
-  //       //   passwordErr : true
-          
-  //       // })
-  //       setError({passWordErr   : 'Please fill password field!'})
-  //       // seterrorMessage({passWordErr   : 'Please fill password field!'})
-  //     } else if (passWord.length < 6) {
-  //       // setError({
-  //       //   usernameErr: false,
-  //       //   firstnameErr : false,
-  //       //   lastameErr : false,
-  //       //   passwordErr : true
-          
-  //       // })
-  //       // seterrorMessage({passwordErr   : 'Password must be at least 6 characters long!'})
-  //       setError({passwordErr   : 'Password must be at least 6 characters long!'})
-  //     } else {
-  //       console.log ('password created')
-  //     }
-  //   //end of internal ifs  
-  //  if ((userName === '') && (firstName === '') && (lastName === '') && (passWord === '')) {
-  //     alert ('All fields are empty')
-  //     console.log ('invalid form')
-  // } 
+//Save validated account
+const saveAccount = () => {
 
-} //closing of validateUser
-
-//Save validated 
-function saveAccount() {
-    
   if ((userName !== '') && (firstName !== '') && (lastName !== '') && (passWord !== '')) {
+    
     
     if (form_valid===true) {
 
@@ -204,95 +78,110 @@ function saveAccount() {
       history.push('/accounts') 
       alert(`User ${userName} succesfully created!`)
     } else {   
+          form_valid=false
           alert(`Invalid!`)
     }     
- }
-}
+  } else {
+      form_valid=false
+      alert ('Please fill required fields!')
+  }
+} 
 
-  //form validator
+  //form validator checks/counts errors
   const validateForm = (error) => {
     let valid=true;
     Object.values(error).forEach(
       (val) => val.length > 0 && (valid = false)
     );
+    if(valid === true){
+      console.log("Registering can be done");
+   }else{
+      console.log("You cannot be registered!!!")
+   }
     return valid;
-  }
+}
+  
+//onChange function
+const handleChange = (e, state) => { 
 
-
-  const handleSubmit = (e) => {
-      e.preventDefault();
-      // validateUser()
-          if (validateForm(error)) {
-            console.info('Valid Form')
-            form_valid=true
-          } else{
-            console.error('Invalid Form')
-            form_valid=false
-          }
-          saveAccount()
-        } 
-
-
-  let handleChange = (e, state) => { 
-    // e.preventDefault();
- 
     switch (state) {
       case 'userName':
-        setuserName(e.target.value)
-        validateUser()
+        validateUser(e) 
         error.usernameErr = 
-          // userName === ''
-          // ? setError({usernameErr   : 'Username must not be empty'})
-          // :
-          userName.length < 5
+          e.target.value === ''
+          ? setError({usernameErr   : 'Username must not be empty'})
+          :
+          e.target.value.length < 5
           ? setError({usernameErr   : 'Username must be at least 5 characters long'})
           :
-          user_exist===1
-          // user_exist === true
+          user_exist===true
           ? setError({usernameErr   : 'Username already exist'})
-          : '';
+          : ''
+          setuserName(e.target.value)
         break;
       case 'firstName' :
-        setfirstName(e.target.value);
         error.firstnameErr = 
-          validNameRegex.test(firstName)
+          e.target.value === ''
+          ? setError({firstnameErr   : 'Firstname must not be empty'})
+          :
+          validNameRegex.test(e.target.value)
           ? setError({firstnameErr   : 'First name must not include numbers and special characters!'})
           :
-          firstName.length < 2
+          e.target.value.length < 2
           ? setError({firstnameErr   : 'Firstname must be at least 2 characters long'})
-          : '';
+          : ''
+          setfirstName(e.target.value)
         break;
-      case 'lastName' :
-        setlastName(e.target.value);
+      case 'lastName':
+        error.lastnameErr =
+        e.target.value === ''
+        ? setError({lastnameErr   : 'Lastname must not be empty'})
+        : 
+        (e.target.value.length < 2)
+        ? setError({lastnameErr   : 'Last name must be at least 2 characters long'})
+        :
+        (validNameRegex.test(e.target.value))
+        ? setError({lastnameErr   : 'Must not include numbers and special characters!'})
+        :''
+        setlastName(e.target.value)
         break;
       case 'passWord' :
-        setpassWord(e.target.value);
+        error.passwordErr =
+        e.target.value === ''
+        ? setError({passwordErr   : 'Password must not be empty'})
+        : 
+        (e.target.value.length < 6)
+        ? setError({passwordErr   : 'Password must be at least 6 characters long!'})
+        :''
+        setpassWord(e.target.value)
         break;
       default:
-        break;
+        break
     }
-      // console.log(userName)
-    }
-
-
-  // function saveAccount() {
     
-  //   // validation()
-  //     // if ((username_input !== '') && (user_valid === true) && (firstname_input !== '') && (lastname_input !== '') && (password_input !=='')) {
-  //     if (form_valid ===true) {
+ 
+    // console.log(userName)
+}
 
-  //       adminInfo.firstname = document.getElementById('admin-firstname').value;
-  //       adminInfo.lastname = document.getElementById('admin-lastname').value;
-  //       adminInfo.password = document.getElementById('admin-password').value;
-  //       localStorage.setItem(userName, JSON.stringify(adminInfo)); 
-  //       history.push('/accounts') 
-  //       alert(`User ${userName} succesfully created!`)
-  //     } else {   
-  //           alert(`Please check errors!`)
-  //     }     
-  // }     
+//on submit handle validation
+const handleSubmit = (e) => {
+  e.preventDefault();
+  // validateUser()
+      if (validateForm(error))  {
+        // console.info('Valid Form')
+        form_valid = true
+        console.info('Form submitted')
+        saveAccount()
+      } 
+     
+      else{
+        form_valid = false
+        // console.error('Invalid Form')
+        console.error('Form has errors')
+      }
+} 
 
-  
+
   return (
 
   <> 
@@ -300,39 +189,32 @@ function saveAccount() {
     <div className='wrapper'>
         <div className='form-wrapper'>
           <h2>Register Admin</h2>
+          
           <form onSubmit={handleSubmit}>
 
           <div className='fullName'>
             <Textfield id="admin-username" type="text" onChange={(e) => handleChange (e, 'userName')} value={userName}>User Name</Textfield>
             {/* <Error classnames={error.usernameErr === true ? 'errortext' : 'hide'}>{error.usernameErr}</Error> */}
-              {/* <label className="required" htmlFor="username">User Name</label>
-              <input type='text' id='admin-username' name='username' onChange={handleChange}/> */}
-              {/* {error.usernameErr >= 0 && <span className='error'>{error.usernameErr}</span>}           */}
-              {error.usernameErr !== '' && <span className='error'>{error.usernameErr}</span>} 
+            {error.usernameErr !== '' && <span className='error'>{error.usernameErr}</span>} 
             </div>
 
             <div className='fullName'>
             <Textfield id="admin-firstname" type="text"  onChange={(e) => handleChange (e, 'firstName')} value={firstName}>First Name</Textfield>
             {/* <Error classnames={error.firstnameErr === true ? 'errortext' : 'hide'}>{error.firstnameErr}</Error> */}
-              {/* <label htmlFor="firstname">First Name</label>
-              <input type='text' id='admin-firstname' name='firstname' onChange={handleChange}  /> */}
                {error.firstnameErr !== '' && <span className='error'>{error.firstnameErr}</span>} 
             </div>
 
             <div className='fullName'>
             <Textfield id="admin-lastname" type="text" value={lastName} onChange={(e) => handleChange (e, 'lastName')}>Last Name</Textfield>
-            <Error classnames={error.lastameErr === true ? 'errortext' : 'hide'}>{error.lastameErr}</Error>
-              {/* <label htmlFor="lastname">Last Name</label>
-              <input type='text' id='admin-lastname' name='lastname' onChange={handleChange}  /> */}
-              {/* {errors.lastname.length > 0 && <span className='error'>{errors.lastname}</span>} */}
+            {/* <Error classnames={error.lastameErr === true ? 'errortext' : 'hide'}>{error.lastameErr}</Error> */}
+              {error.lastnameErr !== '' && <span className='error'>{error.lastnameErr}</span>}
             </div>
 
             <div className='password'>
             <Textfield id="admin-password" type="password" value={passWord} onChange={(e) => handleChange (e, 'passWord')}>Password</Textfield>
-            <Error classnames={error.passwordErr === true ? 'errortext' : 'hide'}>{error.passwordErr}</Error>
-              {/* <label htmlFor="password">Password</label>
-              <input type='password' id='admin-password'name='password' onChange={handleChange}  /> */}
-              {/* {errors.password.length > 0 && <span className='error'>{errors.password}</span>} */}
+            {/* <Error classnames={error.passwordErr === true ? 'errortext' : 'hide'}>{error.passwordErr}</Error> */}
+              {error.passwordErr !== '' && <span className='error'>{error.passwordErr}</span>}
+              {/* {error.passwordErr > 0 && <span className='error'>{error.passWord}</span>} */}
             </div>
    
             <div className='info'>
@@ -346,6 +228,10 @@ function saveAccount() {
       </div>
   </>
   )
+
+
+
+
 }
 
 export default Register
