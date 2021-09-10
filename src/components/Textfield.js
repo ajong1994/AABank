01da71e7e8d579735b278d609588f3b67af44722
Alex Ong <aji@Alexs-MacBook-Pro.local>
2021-09-11 00:29:47 +0800

    Updated node modules, completed Login js CSS, worked on Accounts js CSS and added Error styling

--- src/components/Textfield.js
+++ src/components/Textfield.js
@@ -3,12 +3,11 @@ const Textfield = ({children, id, classnames, placeholder, type, value, onChange
 
 
     return (
-        <div>
-            <label htmlFor={id} className="text-gray-700">{children}</label>
-            {/* <input type={type} name={id} id={id} className={classnames} placeholder={placeholder} required={required}/>                                                                                 */}
+        <>
+            <label htmlFor={id} className="text-gray-700 text-sm">{children}</label>
             <input type={type} name ={id} id={id} placeholder={placeholder} value={value} onChange={onChange} min={min}
-             className="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"/>                                                                                
-        </div>
+             className="mt-1 mb-2 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"/>                                                                                
+        </>
     )
 }
 
