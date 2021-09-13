35419b64916e8ba95d35b21efc1a89614f6077d2
Alex Ong <aji@Alexs-MacBook-Pro.local>
2021-09-14 00:59:59 +0800

    Edited Header component CSS classes to be full height and sticky so it will follow scroll. Also edited Account and Accounts to have 2 column layout and built skeleton for Account page.

--- src/parts/Send.js
+++ src/parts/Send.js
@@ -3,6 +3,8 @@ import Button from '../components/Button'
 import Modal from './Modal'
 import {get_balance} from '../utils/GetBalanceUtil'
 import {formatMoney} from '../utils/FormatMoneyUtil'
+import Form from '../components/Form'
+import Error from '../components/Error'
 
 const Send = ({modalStat, customerData, receivingAccount, sendAmount, onChangeAmount, onChangeAccount, handleModalClose, handleModalOpen, handleSend, accErr, amtErr}) => {
 
@@ -10,28 +12,32 @@ const Send = ({modalStat, customerData, receivingAccount, sendAmount, onChangeAm
     const formattedSendAmt = formatMoney(sendAmount);
 
     return (
-        <div>
             <div className="transaction-container"> 
-            <Textfield id="recipient-input" placeholder="Enter receiving account number" type="number" onChange={onChangeAccount} value={receivingAccount} />
-            <p className={`${accErr ? 'show' : 'hidden'} error`}>Account number must not be blank.</p>
-            <Textfield id="transfer-input" placeholder="Enter your transfer amount" type="number" value={sendAmount} onChange={onChangeAmount} min={0}>Send</Textfield>
-            <p className={`${amtErr ? 'show' : 'hidden'} error`}>Amount must not be blank.</p>
-            <Button type="submit"onclick={() => handleModalOpen({
-                show: true, 
-                status: 'confirmation',
-                deposit: false,
-                withdrawal: false,
-                send: true
-            })}>Send Amount</Button>
+                <Form classnames='h-full'>
+                    <div className='flex flex-col h-full'>
+                        <h4 className='mb-4'>Transfer Money</h4>
+                        <Textfield id="recipient-input" placeholder="Enter receiving account number" type="number" onChange={onChangeAccount} value={receivingAccount}>Account</Textfield>
+                        <Error classnames={accErr ? 'show' : 'hide'}>Account number must not be blank.</Error>
+                        <Textfield id="transfer-input" placeholder="Enter transfer amount" type="number" value={sendAmount} onChange={onChangeAmount} min={0}>Amount</Textfield>
+                        <Error classnames={amtErr ? 'show' : 'hide'}>Amount must not be blank.</Error>
+                        <Button classnames='justify-self-end self-end mt-auto bg-primary text-white py-1 px-2 rounded text-sm' type="submit"onclick={() => handleModalOpen({
+                            show: true, 
+                            status: 'confirmation',
+                            deposit: false,
+                            withdrawal: false,
+                            send: true
+                        })}>Send Amount</Button>
+                    </div>
+
+                </Form>
+                <Modal header="Transfer" show={modalStat.show && modalStat.send ? 'show':'hide'} status={modalStat.status} 
+                    buttonClick={handleSend} onClose={handleModalClose}> 
+                    {modalStat.status === 'confirmation' ? 
+                        (`You are Transferring ${formattedSendAmt} from Account Number: ${customerData.accNum} to Account Number: ${receivingAccount}.`) :
+                        (`Transfer successful! Account balance is now ${formattedBalance}`)
+                    }
+                </Modal> 
             </div>
-            <Modal header="Transfer" show={modalStat.show && modalStat.send ? 'show':'hide'} status={modalStat.status} 
-            buttonClick={handleSend} onClose={handleModalClose}> 
-            {modalStat.status === 'confirmation' ? 
-                (`You are Transferring ${formattedSendAmt} from Account Number: ${customerData.accNum} to Account Number: ${receivingAccount}.`) :
-                (`Transfer successful! Account balance is now ${formattedBalance}`)
-            }
-            </Modal> 
-        </div>
     )
 }
 
