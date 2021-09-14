b0fd99e96f3c4ffea49df64f74f2857fda6c1fb8
Alex Ong <aji@Alexs-MacBook-Pro.local>
2021-09-14 11:58:48 +0800

    Finished UI for account page

--- src/parts/Deposit.js
+++ src/parts/Deposit.js
@@ -17,7 +17,7 @@ const Deposit = ({modalStat, customerData, depositAmount, onChange, handleModalC
                 <Form classnames='h-full'>
                     <div className='flex flex-col h-full'>
                         <h4 className='mb-4'>Cash In</h4>
-                        <Textfield id="deposit-input" placeholder="Enter deposit amount" type="number" value={depositAmount} onChange={onChange} min={0}>Amount</Textfield>
+                        <Textfield id="deposit-input" placeholder="Enter deposit amount" type="number" value={depositAmount} onChange={onChange} min={0}>Deposit Amount</Textfield>
                         <Error classnames={error ? 'show' : 'hide'}>Amount must not be blank.</Error>
                         <Button classnames='justify-self-end self-end mt-auto bg-primary text-white py-1 px-2 rounded text-sm' type="submit" onclick={() => handleModalOpen({
                             show: true, 
@@ -31,8 +31,8 @@ const Deposit = ({modalStat, customerData, depositAmount, onChange, handleModalC
                 <Modal header="Deposit" show={modalStat.show && modalStat.deposit ? 'show':'hide'} status={modalStat.status} 
                     buttonClick={handleDeposit} onClose={handleModalClose}> 
                     {modalStat.status === 'confirmation' ? 
-                        (`You are depositing ${formattedDeposit} to Account Number: ${customerData.accNum}`) :
-                        (`Deposit successful! Account balance is now Php ${formattedBalance}`)
+                        (`You are depositing ${formattedDeposit} to Account Number: ${customerData.accNum}.`) :
+                        (`Deposit successful! Account balance is now Php ${formattedBalance}.`)
                     }
                 </Modal> 
             </div>
